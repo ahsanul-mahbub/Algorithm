a563e4dd10a1b274b8006ad8285fcee4ee4d663c
Ahsanul Mahbub Farhan <89720951+ahsanul-mahbub@users.noreply.github.com>
2024-06-23 23:17:06 +0600

    Update and rename Sieve of Eratosthenes .py to Sieve of Eratosthenes.py
    The Sieve of Eratosthenes is a highly efficient algorithm used to find all prime numbers in the range from 2 to n. By iteratively marking the multiples of each prime starting from 2, it effectively sieves out composite numbers, ensuring optimal performance with a time complexity of  O(n log log n). This makes it a preferred choice for applications requiring fast prime number generation.

--- Sieve of Eratosthenes.py
+++ Sieve of Eratosthenes.py
@@ -14,3 +14,7 @@ for i in range(2, n + 1):
         p_n.append(i)
 
 print(p_n)
+
+___________________________________
+SieveofEratosthenes notebook (https://colab.research.google.com/drive/1aFgAK4e1vb8T9yZ6-EBm8JV-T5Pp2f-q#scrollTo=KswfG2TSVumH)
+Tutorial link (https://youtu.be/0T1-IjBM9jA?si=l60bZDJCkCkqlWrG)
